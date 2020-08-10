41e68f94bc5e0e3e02f6ee99a9defe9f696a7564
duongdonguyen96 <duongdonguyen96@gmail.com>
2020-08-10 08:36:07 +0700

    Acessmodifier BT

---
+++ Month - 2 -REAL/Week1/OOP/[BT] Access modifier/src/Cirle.java
@@ -0,0 +1,22 @@
+public class Cirle {
+    private double radius = 1.0;
+    private String color = "red";
+    public final float PI = 3.14f;
+
+    public Cirle() {
+
+    }
+
+    public Cirle(double radius) {
+        this.radius = radius;
+    }
+
+    protected double getRadius() {
+        return this.radius;
+    }
+
+    protected double getArea() {
+        return this.getRadius() * this.getRadius() * PI;
+    }
+
+}
---
+++ Month - 2 -REAL/Week1/OOP/[BT] Access modifier/src/TestCirle.java
@@ -0,0 +1,9 @@
+public class TestCirle {
+    public static void main(String[] args) {
+        Cirle cirle1 = new Cirle(5.7);
+        cirle1.getRadius();
+        cirle1.getArea();
+        System.out.println("ban kinh la: " + cirle1.getRadius());
+        System.out.println("Dien tich la: " + cirle1.getArea());
+    }
+}
