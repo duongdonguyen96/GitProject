5f5ff4f006b955378f85ac6a42ae33c3df66c384
duongdonguyen96 <duongdonguyen96@gmail.com>
2020-08-21 08:35:03 +0700

    Optional-sapXepNoiBot

---
+++ Month - 2 -REAL/Week3/ThuatToanSapXep/[Optional]ThuaToanSapXepNoiBot/src/Noibot.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Noibot {
+    public static void main(String[] args) {
+
+        int[] list = {100, -100, 200, -200, 10, 20, 50, -10, 0, -10};
+        System.out.println("Mang ban dau :");
+        System.out.println(Arrays.toString(list));
+        sapXepNoiBot(list);
+        System.out.println("Sap xep tu be den lon: ");
+        System.out.println(Arrays.toString(list));
+
+
+    }
+
+    public static void sapXepNoiBot(int[] array) {
+        boolean needNextPass = true;
+        for (int i = 1; i < array.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+
+        }
+
+    }
+}
+
