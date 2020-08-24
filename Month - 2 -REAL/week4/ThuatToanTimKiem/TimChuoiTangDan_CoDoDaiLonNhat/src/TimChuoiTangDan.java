cd772914353cd148c5d090846f38e102c0ed2615
duongdonguyen96 <duongdonguyen96@gmail.com>
2020-08-24 09:03:08 +0700

    chuoiTangDan-coDoDaiLonNhat

---
+++ Month - 2 -REAL/week4/ThuatToanTimKiem/TimChuoiTangDan_CoDoDaiLonNhat/src/TimChuoiTangDan.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TimChuoiTangDan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Character> maxList = new LinkedList<>();
+        System.out.println("Nhap vao chuoi :");
+        String string = scanner.nextLine();
+
+
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int k = i + 1; k < string.length(); k++) {
+                if (string.charAt(k) > list.getLast()) {
+                    list.add(string.charAt(k));
+                }
+            }
+            if (list.size() > maxList.size()) {
+                maxList.clear();
+                maxList.addAll(list);
+            }
+            list.clear();
+        }
+        System.out.println("Chuoi tang dan");
+        System.out.println(maxList);
+
+    }
+}
