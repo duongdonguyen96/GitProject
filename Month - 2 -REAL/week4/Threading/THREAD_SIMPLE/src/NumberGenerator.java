156680bc54f131fbcb70725760f371d983b32d72
duongdonguyen96 <duongdonguyen96@gmail.com>
2020-08-28 00:00:59 +0700

    THREAD_SIMPLE

---
+++ Month - 2 -REAL/week4/Threading/THREAD_SIMPLE/src/NumberGenerator.java
@@ -0,0 +1,23 @@
+public class NumberGenerator implements Runnable {
+    private String name;
+
+    public NumberGenerator() {
+    }
+
+    public NumberGenerator(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(this.name + "= " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
